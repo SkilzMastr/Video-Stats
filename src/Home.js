ad8a59ece32216d507bb2b9f17b4b28f21ab5ff7
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2021-01-02 13:20:57 -0500

    Added Multiple Page Support

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Findvid from './Findvid'
+import './App.css'
+
+export default function Home() {
+    return (
+        <div>
+            <Findvid/>
+        </div>
+    )
+}
