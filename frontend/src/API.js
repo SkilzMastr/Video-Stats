cd5b0ac5ac157754de69f49448c775f15673ac54
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2021-01-03 23:13:37 -0500

    Kill Me

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,4 +1,4 @@
-import React, { Component, useRef} from 'react'
+import React, { Component } from 'react'
 const axios = require('axios')
 require('dotenv').config();
 
@@ -9,8 +9,8 @@ export default class API extends Component {
 
     constructor() {
         super();
+        this.urlRef = React.createRef();
         this.state = {
-            urlRef: useRef(),
             views: "Channel Info"
         };
     }
@@ -18,16 +18,23 @@ export default class API extends Component {
 
 
     res = ()=> {
-        axios.get(`process.env.API_URL || http://localhost:5000/video/${this.state.id}`).then((response)=>{
+        axios.get(process.env.API_URL || `http://localhost:5000/video/${this.removeData()}`).then((response)=>{
             console.log(response.data.items["statistics"].viewCount)
         })
+    }
+
+    removeData = ()=>{
+        var url = this.urlRef.current.value
+        return url.slice(32, url.length)
     }
     
     render() {
         return (
             <div>
-                <input ref={urlRef} type="text" placeholder="Enter YouTube URL"/>
-                <button onClick={this.res} id="submit">Search</button>
+                <label>
+                    <input ref={this.urlRef} type="text" placeholder="Enter YouTube URL"/>
+                    <button onClick={this.res} id="submit">Search</button>
+                </label>
             </div>
         )
     }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css';
-import Findvid from './Findvid'
+import API from './API';
 
 export default function App() {
   return (
       <div className="App">
         <header className="App-header"/>
-        <Findvid/>
+        <API/>
       </div>
   );
 }
